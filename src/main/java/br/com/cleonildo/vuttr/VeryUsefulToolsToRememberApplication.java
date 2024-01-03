15a876ad2fad8aa9de2e5e9eb637042dfde614ef
CleuJunior <cleu.junior@gmail.com>
2024-01-03 16:47:22 -0300

    feat: initial commit

---
+++ src/main/java/br/com/cleonildo/vuttr/VeryUsefulToolsToRememberApplication.java
@@ -0,0 +1,13 @@
+package br.com.cleonildo.vuttr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeryUsefulToolsToRememberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeryUsefulToolsToRememberApplication.class, args);
+	}
+
+}

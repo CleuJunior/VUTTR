f3699adedff391953a47fec41a8d2de079355380
CleuJunior <cleu.junior@gmail.com>
2024-01-05 18:09:41 -0300

    feat: Swagger doc for the endpoints

--- src/main/java/br/com/cleonildo/vuttr/VeryUsefulToolsToRememberApplication.java
+++ src/main/java/br/com/cleonildo/vuttr/VeryUsefulToolsToRememberApplication.java
@@ -1,9 +1,12 @@
 package br.com.cleonildo.vuttr;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Very Useful Tools to Remember", version = "1"))
 public class VeryUsefulToolsToRememberApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/cleonildo/vuttr/controllers/ToolController.java
+++ src/main/java/br/com/cleonildo/vuttr/controllers/ToolController.java
@@ -3,6 +3,13 @@ package br.com.cleonildo.vuttr.controllers;
 import br.com.cleonildo.vuttr.dto.ToolRequest;
 import br.com.cleonildo.vuttr.dto.ToolResponse;
 import br.com.cleonildo.vuttr.services.ToolService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,10 +25,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
-@RestController
-@RequestMapping(value = "/tools")
 @Controller
+@RestController
+@RequestMapping(value = "/tools", produces = "application/json")
+@Tag(name = "vuttr")
 public class ToolController {
 
     private final ToolService service;
@@ -31,34 +38,60 @@ public class ToolController {
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<ToolResponse> getToolById(@PathVariable Integer id) {
-        return ResponseEntity.status(HttpStatus.OK).body(this.service.getToolById(id));
+    @Operation(summary = "Search data from a tool by id", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Search completed successfully"),
+            @ApiResponse(responseCode = "404", description = "Not found")
+    })
+    public ResponseEntity<ToolResponse> getToolById(@PathVariable(value = "id") Integer id) {
+        var body = this.service.getToolById(id);
+        return ResponseEntity.ok(body);
     }
 
+    @Operation(summary = "Search the full list of tools or search by tag", method = "GET")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Search completed successfully")})
     @GetMapping
     public ResponseEntity<List<ToolResponse>> getListTools() {
-        return ResponseEntity.status(HttpStatus.OK).body(this.service.getAllTools());
+        var body = this.service.getAllTools();
+        return ResponseEntity.ok(body);
     }
 
+    @Parameters(@Parameter(name = "tag", description = "Name of the tag to be searched"))
     @GetMapping(params = "tag")
-    public ResponseEntity<List<ToolResponse>> getListToolsByTag(@RequestParam String tag) {
-        return ResponseEntity.status(HttpStatus.OK).body(this.service.getAllToolsByTag(tag));
+    public ResponseEntity<List<ToolResponse>> getListToolsByTag(@RequestParam(value = "tag", required = false) String tag) {
+        var body = this.service.getAllToolsByTag(tag);
+        return ResponseEntity.ok(body);
     }
 
+    @Operation(summary = "Save new tool", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "New tool saved"),
+            @ApiResponse(responseCode = "400", description = "Bad request from user")
+    })
     @PostMapping
     public ResponseEntity<ToolResponse> saveTool(@RequestBody @Valid ToolRequest request) {
-        var response = this.service.saveTool(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        var body = this.service.saveTool(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(body);
     }
 
+    @Operation(summary = "Update an existing tool", method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Tool updated successfully"),
+            @ApiResponse(responseCode = "400", description = "Bad request from user")
+    })
     @PutMapping(value = "/{id}")
     public ResponseEntity<ToolResponse> updatePatient(@PathVariable Integer id,
                                                       @RequestBody @Valid ToolRequest request) {
 
-        var response = this.service.updateTool(id, request);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        var body = this.service.updateTool(id, request);
+        return ResponseEntity.ok(body);
     }
 
+    @Operation(summary = "Delete an existing tool", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Tool deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Not found")
+    })
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> deletePatient(@PathVariable Integer id) {
         this.service.deleteToolById(id);
